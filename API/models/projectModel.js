ff64da46a6123582acf6c373246fc23f7192bca3
Aryan Sharma <78246636+AryanSharma02@users.noreply.github.com>
2025-05-25 15:55:54 +0530

    Project Schema few edits

--- API/models/projectModel.js
+++ API/models/projectModel.js
@@ -8,6 +8,7 @@ const projectSchema = mongoose.Schema({
     description : String,
     startDate : Date,
     endDate : Date,
+    //team will have array employee documents from Employee Collecitons
     team : [{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Employee'
