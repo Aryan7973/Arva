72e95c84aabe54228bc1083567c99ea076a3e9a4
Vaibhav Sharma <129342807+vibhucodes-debug@users.noreply.github.com>
2025-05-29 06:39:53 +0000

    Created first Employee document on mongo db.

--- API/models/employeeModel.js
+++ API/models/employeeModel.js
@@ -1,85 +1,78 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose')
 
-mongoose.connect('',{useNewUrlParser:true, useUnifiedTopology:true}),then(()=>{
-    console.log('Connected to MongoDB')
-}).catch(error => console.log('Problem connecting'))
+const employeeSchema = new mongoose.Schema({
 
-// connection  ke liye alag banao bhai file hehe
-// Baad mai server.js mai daal denge
+    employeeId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    jobeRole: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    reportingManager: {
+        type: String,
+        require: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    projectCode: {
+        type: String,
+        required: true
+    },
+    joiningDate: {
+        type: Date,
+        required: true
+    },
+    companyEmail: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    personalEmail: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ['M', 'F', 'O'],
+        uppercase: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8
+    },
+    workShift: {
+        type: String,
+    }
+},
+    {
+        timestamps: true
+    });
 
-const employeeSchema = mongoose.Schema({
+const Employee = mongoose.model('Employee', employeeSchema);
 
-employeeId:{
-    type:String,
-    required:true,
-    unique:true
-},
-firstName:{
-    type:String,
-    required:true
-},
-lastName:{
-    type:String,
-    required:true
-},
-jobeRole:{
-    type:String,
-    required:true
-},
-dateOfBirth:{
-    type:Date,
-    required:true
-},
-reportingManager:{
-    type:String,
-    require:true
-},
-department:{
-    type:String,
-    required:true
-},
-projectCode:{
-    type:String,
-    required:true
-},
-joiningDate:{
-    type:Date,
-    required:true
-},
-companyEmail:{
-    type:String,
-    required:true,
-    unique:true
-},
-personalEmail:{
-    type:String,
-    required:true,
-    unique:true
-},
-gender:{
-    type:String,
-    required:true,
-    enum:['M','F','O'],
-    uppercase:true
-},
-phoneNumber:{
-    type:Number,
-    required:true,
-    unique:true
-},
-password:{
-    type:String,
-    required:true,
-    minLength:8
-},
-workShift:{
-    type:String,
-}
-
-},{
-    timestamps:true
-});
-
-const employee = mongoose.model('Employee',employeeSchem);
-
-export default employee;
+module.exports = Employee
