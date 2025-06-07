2978f67da9a4441b403bb8365c53a439d2c9fa46
Vaibhav Sharma <vaibhavsharma2124@gmail.com>
2025-06-07 12:36:01 +0530

    Sign In Page Working

--- App/src/components/LoginPage.jsx
+++ App/src/components/LoginPage.jsx
@@ -5,25 +5,25 @@ import Popup from "./Popup.jsx"
  
 const LoginForm = () => {
 
-    const [email, setEmail] = useState('');
+    const [companyEmail, setcompanyEmail] = useState('');
     const [password, setPassword] = useState('');
     const [responseMessage, setResponseMessage] = useState('');
     const [showPopup, setShowPopup] = useState(false);
 
     const handleSubmit = async(e)=>{
-        console.log("Submit clicked!")
         e.preventDefault();
 
         const backendUrl = 'http://localhost:5000/api/auth/signin';
 
         try{
-            const response = await axios.post(backendUrl,{email,password});
-            setResponseMessage(`Success: ${response.data.message}`);
+            const response = await axios.post(backendUrl,{companyEmail,password});
+            console.log(response)
+            setResponseMessage(`Success: ${response.data.msg}`);
             setShowPopup(true);
-            console.log(`Backend Response: ${response.data}`);
+            console.log(response.data)
         }catch(error){
             if(error.response){
-                setResponseMessage(`Error: ${error.response.message || 'Someting went wrong!'}`);
+                setResponseMessage(`${error.response.data.msg || 'Someting went wrong!'}`);
             }else if(error.request){
                 setResponseMessage('Error: No response from server. Please check your network connection or backend server status.');
             }else{
@@ -46,7 +46,7 @@ const LoginForm = () => {
                     <div className="login-form">
                         <h2>Login to <span style={{ 'color': '#205295', 'fontSize': '18px' }}>ARVA</span></h2>
                         <form>
-                            <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter organisation email" required />
+                            <input type="email" value={companyEmail} onChange={(e)=>setcompanyEmail(e.target.value)} placeholder="Enter organisation companyEmail" required />
                             <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter password" required />
                         </form>
                         <p>Forgot password?</p>
